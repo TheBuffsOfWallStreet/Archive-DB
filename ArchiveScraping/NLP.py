13bc7c5f6951c465e34e476d2960e03c12478d29
Royce Schultz <rosc1351@colorado.edu>
2020-09-28 17:17:52 -0600

    Adds sentiment detector with textblob.

--- ArchiveScraping/NLP.py
+++ ArchiveScraping/NLP.py
@@ -1,4 +1,5 @@
 import spacy as sp
+from textblob import TextBlob
 
 nlp = sp.load("en_core_web_sm")
 
@@ -18,3 +19,11 @@ def getEntities(text, selected_entities = {'ORG', 'PERSON'}):
         # Convert to list so it's json serializable.
         entities[ent] = list(entities[ent])
     return entities
+
+
+def getSentiment(text):
+    '''
+    A simple wrapper for TextBlob sentiment detector.
+    Returns 2 values, polarity (-1,1) and subjectivity (0,1).
+    '''
+    return TextBlob(text).sentiment
