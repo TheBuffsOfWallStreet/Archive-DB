13a86ad1e68adc443c86bb516b1928df76294ff3
Royce Schultz <rosc1351@colorado.edu>
2020-09-29 00:53:54 -0600

    Adds basic tests and __init__ files to create testing directory structure.

---
+++ ArchiveScraping/test/test_import.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def test_Archive_is_importable():
+    from ArchiveScraping import Archive
+
+
+def test_Segment_is_importable():
+    from ArchiveScraping import Segment
+
+
+def test_NLP_is_importable():
+    from ArchiveScraping import NLP
