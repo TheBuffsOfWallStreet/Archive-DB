db6abfd2a30f811385fe145cead27a377e7da306
Royce Schultz <rosc1351@colorado.edu>
2020-10-08 16:13:27 -0600

    Adds new options to DBMan. User can clean data and limit the number of episodes downloaded

--- DBMan.py
+++ DBMan.py
@@ -1,17 +1,33 @@
 if __name__ == '__main__':
     from db import StoreArchive
+    from db import CleanArchive
+
+    def myInput(text):
+        return input(f'\033[92m {text} \033[0m')
 
     menu = None
     while menu != 'q':
         print()
         print('i) Build index')
         print('d) Download episodes')
+        print('c) Clean Data')
         print('q) Quit')
+        print()
 
-        menu = input('\n Selection:')
+        menu = myInput('Selection:')
         if menu == 'i':
             StoreArchive.buildIndex()
         elif menu == 'd':
-            StoreArchive.buildEpisodes()
+            n = myInput('How Many?:')
+            if n.isdigit():
+                StoreArchive.buildEpisodes(int(n))
+            elif n == 'all':
+                StoreArchive.buildEpisodes()
+            else:
+                print(' Invalid Input. Enter number or `all`.')
+        elif menu == 'c':
+            CleanArchive.clean()
+        elif menu == 'q':
+            print(' Goodbye!')
         else:
             print(' Invalid Input')
