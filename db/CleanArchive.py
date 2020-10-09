95559eaf97c67c1c7de15414b7a7262b34905eb5
Royce Schultz <rosc1351@colorado.edu>
2020-10-09 00:12:53 -0600

    adds docstings and comments

--- db/CleanArchive.py
+++ db/CleanArchive.py
@@ -5,6 +5,7 @@ db = MongoClient('localhost', 27017).WallStreetDB
 
 
 def cleanIndexDates(episode):
+    '''episode['date'] (str) -> (datetime object) episode['datetime'].'''
     form = '%Y-%m-%dT%H:%M:%SZ'
     update = {'$set': {
         'datetime': datetime.strptime(episode['date'], form)
@@ -13,10 +14,15 @@ def cleanIndexDates(episode):
 
 
 def cleanEpisodeDuration(episode):
+    '''
+    episode['metadata']['Duration'] (str) ->
+        episode['metadata']['Duration_s']
+    If duration is 0, add 'duration_is_0' flag to errors set.
+    '''
     duration = datetime.strptime(episode['metadata']['Duration'], '%H:%M:%S')
     duration_delta = timedelta(hours=duration.hour, minutes=duration.minute, seconds=duration.second)
     update = {'$set': {
-        'Duration_s': duration_delta.seconds
+        'metadata.Duration_s': duration_delta.seconds
     }}
     if duration_delta.seconds == 0:
         update['$addToSet'] = {'errors': 'duration_is_0'}
@@ -24,10 +30,12 @@ def cleanEpisodeDuration(episode):
 
 
 def markEmpty(episode):
+    '''Adds 'transcript_is_empty' flag to a given episode.'''
     db.ArchiveIndex.update_one({'_id': episode['_id']}, {'$addToSet': {'errors': 'transcript_is_empty'}})
 
 
 def perform(cursor, action):
+    '''Wraps cleaning funcitons in a try except loop in case of divide by 0 error or date format changes.'''
     updated, failed = 0, 0
     for episode in cursor:
         try:
@@ -40,24 +48,25 @@ def perform(cursor, action):
 
 
 def clean(all=False):
-    # Convert index datetime (string) to datetime object.
+    '''
+    User function to run all cleaning functions.
+    if all == False, data that has already been scanned is ignored.
+    '''
     print('Cleaning Index Dates')
-    if all:
-        query = {}
-    else:
-        query = {'date': {'$exists': True}, 'datetime': {'$exists': False}}
-    cursor = db.ArchiveIndex.find(query)
-    perform(cursor, cleanIndexDates)
+    query = {'date': {'$exists': True}}
+    if not all:
+        query['datetime'] = {'$exists': False}  # ignore rows that already have datetime fields.
+    perform(db.ArchiveIndex.find(query), cleanIndexDates)
 
     # Convert Episode Duration (string) to an integer representing seconds.
     print('Cleaning Episode Duration')
-    if all:
-        query = {}
-    else:
-        query = {'metadata': {'$exists': True}, 'metadata.Duration_s': {'$exists': False}}
-    cursor = db.ArchiveIndex.find(query)
-    perform(cursor, cleanEpisodeDuration)
+    query = {'metadata': {'$exists': True}}
+    if not all:
+        query['metadata.Duration_s'] = {'$exists': False}
+    perform(db.ArchiveIndex.find(query), cleanEpisodeDuration)
 
+    # This query was generated with MongoDB Compass.
+    # View intermediate results of the pipeline by pasting into Compass.
     empty_transcripts = db.ArchiveIndex.aggregate([
         {
             '$match': {
