691be7165e379e93ea94807196ec96defa0560c2
Andrew Yee <andrewyee@me.com>
2020-10-15 20:18:57 -0600

    add function to count amount contributed by organization

--- CampaignContributions/ContributionAnalysis.py
+++ CampaignContributions/ContributionAnalysis.py
@@ -22,3 +22,37 @@ def importCandidates(filepath):
     
     with open('candidates.json', 'w') as f:
         json.dump(candidates, f)
+        
+
+def countOrgContributions(record_file, contributions_file='contributions.json'):
+    '''
+    Take a contribution from committees file from fec.gov and a contribution json
+    Writes out a contribution json with the organization name as a key to a dictionary
+    of aggregated contributions by candidate
+    '''
+    try:
+        with open(contributions_file, 'r') as f:
+            contributions = json.load(f)
+    except:
+        contributions = {}
+    
+    with open(record_file, 'r') as f:
+        lines = f.readlines()
+        print(lines[0])
+        first = True
+        for line in lines:
+            data = line.strip('\n').split('|')
+            cand_id = data[16]
+            amount = data[14]
+            name = data[7]
+            if first:
+                first = False
+                print(cand_id, amount, name)
+            if name not in contributions:
+                contributions[name] = {}
+            if cand_id not in contributions[name]:
+                contributions[name][cand_id] = 0
+            contributions[name][cand_id] += float(amount)
+    
+    with open(contributions_file, 'w') as f:
+        json.dump(contributions, f)
