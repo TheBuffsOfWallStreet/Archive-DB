27566ace81bf0716e1806daf7e13049ec73a7ce4
Royce Schultz <rosc1351@colorado.edu>
2020-10-23 00:41:27 -0600

    pull bugfix. Reduces memory usage. Document data is stored and released before the thread continues to a new request.

--- runner/src/Database/StoreArchive.py
+++ runner/src/Database/StoreArchive.py
@@ -15,13 +15,23 @@ db = connect()
 
 def buildIndex(network):
     '''Gets, cleans and stores data from archiveIndexGenerator().'''
-    form = '%Y-%m-%dT%H:%M:%SZ'
     for item in fetch.archiveIndexGenerator(network):
         # TODO: Add lastModified field.
         item['_id'] = item['identifier']
         key = {'_id': item['_id']}
         # Upsert inserts (instead of update) if the item does not exist.
         db.ArchiveIndex.update_one(key, {'$set': item}, upsert=True)
+
+
+def writeEpisode(episode_id):
+    '''Use fetch module to get episode data, then write to database.'''
+    try:
+        data = fetch.getEpisode(episode_id)
+        db.ArchiveIndex.update_one({'_id': episode_id}, {'$set': data})
+        return True
+    except Exception as e:
+        logging.exception(e)
+        return False
 
 
 def buildEpisodes(n=None):
@@ -36,18 +46,14 @@ def buildEpisodes(n=None):
         empty_episodes = empty_episodes.limit(n)
 
     with futures.ThreadPoolExecutor(max_workers=8) as executor:
-        future_to_id = {executor.submit(fetch.getEpisode, item['_id']): item['_id'] for item in empty_episodes}
+        future_to_id = {executor.submit(writeEpisode, item['_id']): item['_id'] for item in empty_episodes}
         for i, future in enumerate(futures.as_completed(future_to_id)):
-            try:
-                id = future_to_id[future]
-                data = future.result()
-                db.ArchiveIndex.update_one({'_id': id}, {'$set': data})
+            id = future_to_id[future]
+            succeded = future.result()
+            if succeded:
                 num_suceed += 1
-            except Exception as e:
-                logging.warning(f'Item with id {id} threw exception.')
-                logging.exception(e)
+            else:
                 num_failed += 1
-            finally:
-                print(f'{i+1} documents, {num_failed} failed ({num_failed/(i+1):.0%})', end='\r')
+            print(f'{i+1} documents, {num_failed} failed ({num_failed/(i+1):.0%})', end='\r')
     print()
     print(f'downloaded {num_suceed} new documents')
