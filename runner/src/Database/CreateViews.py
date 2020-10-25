b0974cab0708acd447527f8c3db74b84952646a0
Royce Schultz <rosc1351@colorado.edu>
2020-10-25 17:14:23 -0600

    Improves createViews function

--- runner/src/Database/CreateViews.py
+++ runner/src/Database/CreateViews.py
@@ -3,59 +3,38 @@ from Database.Connect import connect
 db = connect()
 
 
-def createCleanEpisodes():
-    '''CleanEpisodes only contains episodes that have been cleaned and have no errors.'''
-    if "CleanEpisodes" in db.list_collection_names():
-        print('CleanEpisodes already exists')
-        return
+def CreateViewIfNotExists(name, view_on, pipeline):
+    if name in db.list_collection_names():
+        print(f'{name} already exists')
     else:
         db.command({
-            "create": "CleanEpisodes",
-            "viewOn": "Episodes",
-            "pipeline": [
-                {
-                    '$match': {
-                        'errors': {
-                            '$exists': False
-                        },
-                        'transcript_str_length': {
-                            '$exists': True
-                        }
-                    }
-                }
-            ]
+            "create": name,
+            "viewOn": view_on,
+            "pipeline": pipeline
         })
+        print(f'Created view {name} on {view_on}')
 
 
-def createSeriesSummary():
-    '''SeriesSummary stores aggregate information about series like the number of episodes.'''
-    if "SeriesSummary" in db.list_collection_names():
-        print('SeriesSummary already exists')
-        return
-    else:
-        db.command({
-            'create': 'SeriesSummary',
-            'viewOn': 'Episodes',
-            'pipeline': [
-                {
-                    '$group': {
-                        '_id': '$metadata.Title',
-                        'num_episodes': {
-                            '$sum': 1
-                        },
-                        'total_len': {
-                            '$sum': '$transcript_str_length'
-                        },
-                        'total_duration': {
-                            '$sum': '$metadata.Duration_s'
-                        }
-                    }
-                }
-            ]
-        })
+def groupBySummaryPipeline(group_by, push_id=False):
+    group = {
+        '_id': group_by,
+        'num_episodes': {'$sum': 1},
+        'total_len': {'$sum': '$transcript_str_length'},
+        'total_duration': {'$sum': '$metadata.Duration_s'}
+    }
+    if push_id:
+        group['episodes'] = {'$push': '$_id'}
+    return [{'$group': group}]
 
 
 def createViews():
     '''Wrapper that calls all view functions.'''
-    createCleanEpisodes()
-    createSeriesSummary()
+    CreateViewIfNotExists('CleanEpisodes', 'Episodes', [
+        {'$match': {
+            'errors': {'$exists': False},
+            'transcript_str_length': {'$exists': True}
+        }}
+    ])
+    CreateViewIfNotExists('SeriesSummary', 'Episodes', groupBySummaryPipeline('$metadata.Title'))
+    CreateViewIfNotExists('NetworkSummary', 'Episodes', groupBySummaryPipeline('$metadata.Network'))
+    CreateViewIfNotExists('ErrorSummary', 'Episodes', [{'$unwind': {'path': '$errors'}}] + groupBySummaryPipeline('$errors', push_id=True))
