c3a4c63ccaddfbd9998a1bf4620de5266fb86af4
Royce Schultz <rosc1351@colorado.edu>
2020-11-10 01:46:01 -0700

    adds driver code to calcualte and store sentiment scores in the database

--- runner/src/NLP/StoreNLP.py
+++ runner/src/NLP/StoreNLP.py
@@ -1,5 +1,5 @@
 from Database import connect
-from NLP.NLP import getEntities
+from NLP.NLP import getEntities, getSentiment
 from Parallel import runProcesses
 
 
@@ -12,6 +12,24 @@ def storeEntities(episode_id):
 
 
 def runStoreEntities():
+    db = connect()
+    episode_ids = [x['_id'] for x in db.CleanEpisodes.find({}, {'_id': 1})]
+    runProcesses(storeEntities, episode_ids, 4)
+
+
+def storeSentiment(episode_id):
+    db = connect(new=True)
+    episode = db.Episodes.find_one({'_id': episode_id})
+    print(episode_id)
+    setFields = {}
+    for i, snippet in enumerate(episode['snippets']):
+        polarity, subjectivity = getSentiment(snippet['transcript'])
+        setFields[f'snippets.{i}.polarity'] = polarity
+        setFields[f'snippets.{i}.subjectivity'] = subjectivity
+    db.Episodes.update({'_id': episode_id}, {'$set': setFields})
+
+
+def runStoreSentiment():
     db = connect()
     episode_ids = [x['_id'] for x in db.CleanEpisodes.find({}, {'_id': 1})]
     runProcesses(storeEntities, episode_ids, 4)
