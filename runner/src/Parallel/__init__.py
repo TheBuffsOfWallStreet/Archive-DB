9c18d73ae4b3ae7211cfd5ce8b118e0349be36f1
Royce Schultz <rosc1351@login12.rc.int.colorado.edu>
2020-12-01 00:56:56 -0700

    uses chunking to improve performance. Adjusts smoothing to use simple average rather than exponential decay

--- runner/src/Parallel/__init__.py
+++ runner/src/Parallel/__init__.py
@@ -5,9 +5,9 @@ from tqdm import tqdm
 def runProcesses(func, cursor, max_workers=2):
     '''Wrapper for concurrent.furures with helpful performance metrics.'''
     with multiprocessing.Pool(max_workers) as p:
-        bar = tqdm(p.imap(func, cursor, 1),
+        bar = tqdm(p.imap(func, cursor, 10),
                    total=len(cursor),
                    unit='items',
-                   smoothing=0.1)
+                   smoothing=0)
         for _ in bar:
             pass
