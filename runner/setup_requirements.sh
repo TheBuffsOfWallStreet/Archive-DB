cfad5d8ddddcaf13d914720338a13bb95abe122a
Royce Schultz <rosc1351@colorado.edu>
2021-02-16 22:02:34 -0700

    Removes old python runner code

--- runner/setup_requirements.sh
+++
@@ -1,6 +0,0 @@
-pip3 install -r requirements.txt
-
-python3 -m spacy download en_core_web_sm
-
-python3 -m nltk.downloader stopwords
-python3 -m nltk.downloader punkt
--- runner/src/Database/__init__.py
+++
@@ -1,5 +0,0 @@
-from .StoreArchive import buildIndex, buildEpisodes
-from .CleanEpisodes import clean
-from .CreateViews import createViews
-from .CleanDuplicates import cleanDuplicates
-from .Connect import connect
