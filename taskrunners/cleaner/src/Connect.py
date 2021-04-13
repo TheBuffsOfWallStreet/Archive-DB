0173d9c47d3ddc11315f327bd159e32034923774
Royce Schultz <30354775+royceschultz@users.noreply.github.com>
2021-04-12 21:02:53 -0600

    BUG FIX: Makes database connect scripts consistent

--- taskrunners/cleaner/src/Connect.py
+++ taskrunners/cleaner/src/Connect.py
@@ -1,14 +1,16 @@
 from pymongo import MongoClient
+from pymemcache.client.base import Client
+
+db = None
 
 def connect():
+    global db
     domain = 'domain.com'
-    username = 'user'
-    password = 'password'
-    authSource = 'auth'
-    return MongoClient(domain, 27017, username=username, password=password, authSource=authSource).finlab_beta
+    username = 'USERNAME'
+    password = 'PASSWORD'
+    authSource = 'user_table'
+    return MongoClient(domain, 27017, username=username, password=password, authSource=authSource).finlab_prod
 
-
-from pymemcache.client.base import Client
 
 def connect_cache():
     return Client('cache') # Docker DNS routes 'cache' -> memcache
